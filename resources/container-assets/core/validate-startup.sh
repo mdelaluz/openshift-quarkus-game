2f8b55a9314986426fec878bb5d14b00c2a89e50
Heber Romero Tellez <psehgaft@gmail.com>
2026-09-23 13:01:14 -0400

    Implement health check in validate-startup.sh
    Add health check to validate application startup.

--- resources/container-assets/core/validate-startup.sh
+++ resources/container-assets/core/validate-startup.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/env bash
+set -e
 
+STATUS_CODE=$(curl --write-out "%{http_code}" --silent --output /dev/null http://localhost:9080/health || echo "000")
+
+if [ "$STATUS_CODE" -eq 200 ]; then
+    echo "Aplicación iniciada correctamente (HTTP status: 200)"
+    exit 0
+else
+    echo "Error en el arranque de la aplicación (HTTP status: $STATUS_CODE)"
+    exit 1
+fi
